c86ca78d3fa452277fe860e36cf2600f4bfa2dce
ZeyXs <basile.gauttron@gmail.com>
2024-11-08 22:01:07 +0100

    ✨ Ajout du component Logo

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Logo from "$lib/components/Logo.svelte";
+</script>
+
+<div class="bg-hero-pattern bg-cover h-screen">
+
+    <!-- Sidebar -->
+    <div class="fixed h-full w-[30%] right-0 bg-white">
+        <div class="flex flex-row justify-center items-center">
+            <Logo className="h-20" large />
+        </div>
+    </div>
+
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,11 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			backgroundImage: {
+				'hero-pattern': "url('hero-pattern.svg')",
+			},
+		}
 	},
 
 	plugins: []

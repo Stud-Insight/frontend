bdbc97d59a8ed2017f20fac94cb7a5c06ceed24f
ZeyXs <basile.gauttron@gmail.com>
2024-12-12 09:17:45 +0100

    🎨 Amélioration de la syntaxe

--- src/routes/api/account/+server.ts
+++ src/routes/api/account/+server.ts
@@ -1,12 +1,12 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
-    const { password, validationToken } = await request.json();
+    const { email, firstName, lastName } = await request.json();
 
-    const response = await fetch(`http://localhost:8080/auth/activate/${validationToken}`, {
+    const response = await fetch('http://localhost:8080/account', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password })
+        body: JSON.stringify({ email, firstName, lastName })
     });
 
     return json({ success: true });
